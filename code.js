4384f61d09d0b292ed557bc6adf48fc77052c08d
ceREAL133 <72147047+ceREAL133@users.noreply.github.com>
2021-03-23 15:26:35 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,335 @@
+/* let hello = 'hello';
+let owu = 'owu';
+let com = 'com';
+let first = 1;
+let ten = 10;
+let overDofiga = -999;
+let oneTwo = 123;
+let PI = 3.14;
+let sixteen = 16;
+let bool1 = true;
+let bool2 = false;
+
+console.log(`owu = ${owu}`);
+console.log(`sixteen = ${sixteen}`);
+
+alert(`oneTwo = ${oneTwo}`);
+alert(`PI = ${PI}`);
+
+document.write(bool1 + ' ');
+document.write(overDofiga + ' ');
+
+
+
+owu = 'notOWU';
+sixteen = 26;
+oneTwo = '1.2'
+PI = '3.14159'
+bool1 = false;
+overDofiga = 12345;
+
+console.log(`owu = ${owu}`);
+console.log(`sixteen = ${sixteen}`);
+
+alert(`oneTwo = ${oneTwo}`);
+alert(`PI = ${PI}`);
+
+document.write(bool1 + ' ');
+document.write(overDofiga + ' ');
+*/
+
+/*
+let yourName = prompt('Name?');
+let surname = prompt('Surname?');
+let poBatkovi = prompt('po batkovi?');
+
+alert(`name - ${name}`);
+document.write(`surname - ${surname}`);
+console.log(`po batkovi- ${poBatkovi}`);
+
+
+let you = (`${surname} ${yourName} ${poBatkovi}`);
+console.log(`person - ${you}`);
+
+let mumName = prompt('mom\'s Name?');
+let mPoBatkovi = prompt('po batkovi?');
+let mom = (`${surname} ${mumName} ${mPoBatkovi}`);
+console.log(`Mom - ${mom}`);
+
+let papaName = prompt('Papa\'s Name?');
+let fPoBatkovi = prompt('po batkovi?');
+let papa = (`${surname} ${papaName} ${fPoBatkovi}`);
+console.log(`Father - ${papa}`)
+
+//9
+let firstNum = +prompt('First number?');
+let secondNum = +prompt('Second number?');
+let thirdNum = +prompt('Third number?');
+
+console.log(`1 Num = ${firstNum}`);
+console.log(`2 Num = ${secondNum}`);
+console.log(`3 Num = ${thirdNum}`);
+*/
+
+/*
+//8
+let firstNum2 = parseInt(prompt('First number?'));
+let secondNum2 = parseInt(prompt('Second number?'));
+let thirdNum2 = parseInt(prompt('Third number?'));
+let fourthNum2 = parseInt(prompt('Fourth number?'));
+
+const result = firstNum2 + secondNum2 + thirdNum2 + fourthNum2;
+console.log(result);
+
+
+//9
+let firstNumber4 = parseFloat(prompt('Number1'));
+let secondNumber4 = parseFloat(prompt('Number2'));
+
+let result4 = firstNumber4 + secondNumber4;
+console.log(result4);
+
+
+//10
+let firstNumber5 = parseFloat(prompt('Number1'));
+let secondNumber5 = parseFloat(prompt('Number2'));
+let thirdNumber5 = parseFloat(prompt('Number3'));
+
+let result5 = Math.round(firstNumber5 + secondNumber5 + thirdNumber5);
+console.log(result5);
+*/
+
+
+/*
+//11
+let firstNumber3 = parseInt(prompt('Number1'));
+let secondNumber3 = parseInt(prompt('Number2'));
+
+
+let result2 = Math.pow(firstNumber3, secondNumber3);
+console.log(result2);
+*/
+/*
+//12
+let a = 100;
+let b = '100';
+let c = true;
+let d = undefined;
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+*/
+
+/*
+//13
+console.log(Boolean(5/6));
+console.log(Boolean(5 == 6));
+console.log(Boolean(5 === 6));
+console.log(Boolean(5 > 6));
+console.log(Boolean(10 / 10));
+console.log(Boolean(10 == 10));
+console.log(Boolean(10 > 10));
+console.log(Boolean(10 < 10));
+console.log(Boolean(10 % 10));
+console.log(Boolean(123 === '123'));
+console.log(Boolean(123 == '123'));
+ */
+
+//additional
+//false
+//true
+//false
+//true
+//true
+//true
+//true
+//false
+//true
+//true
+//true
+//true
+//true
+//true
+//false
+//true
+//false
+
+/*
+//CLASS
+const str = 'Privet';
+const num = 123;
+const flag = true;
+const txt = 'true';
+
+console.log(typeof(str));
+console.log(typeof(num));
+console.log(typeof(flag));
+console.log(typeof(txt));
+ */
+
+/*
+//2-3
+let a1 = 5 + 3;
+let a2 = 5 - 3;
+let a3 = 5 * 3;
+let a4 = 5 / 3;
+let a5 = 5 % 3;
+let a6 = 5 % 3;
+let a7 = 3 % 5;
+let a8 = 5 + '3';
+let a9 = '5' - 3;
+let a10 = 75 + 'кг';
+
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+console.log(a5);
+console.log(a6);
+console.log(a7);
+console.log(a8);
+console.log(a9);
+console.log(a10);
+ */
+/*
+//4
+const height = '23cm';
+const width = '10cm';
+
+let s = parseInt(width) * parseInt(height);
+console.log(s);
+ */
+
+/*
+//5
+let cHeigh = '10m';
+let dC = '4m';
+let rC = parseInt(dC)/2;
+let v = (Math.PI * Math.pow(rC, 2) * parseInt(cHeigh));
+
+console.log(v);
+ */
+
+/*
+//6
+let n = 3;
+let m = 4;
+
+let c2 = Math.pow(n, 2) + Math.pow(m, 2);
+let c = Math.sqrt(c2);
+
+console.log(c);
+ */
+
+//8
+//alert(`Ileyko Daniel  \n17 \n basketball, knife throwing, reading books`);
+
+/*
+//9
+let str1;
+let str2;
+let str3;
+let concatenation;
+
+str1 = 'хто';
+str2 = 'ти';
+str3 = 'такий?';
+concatenation = `${str1} ${str2} ${str3}`;
+document.write(concatenation);
+*/
+
+/*
+//10
+let str = "20";
+let a = 5;
+document.write(str + a + "<br/>"); //бо конкатенує а до стрінги
+document.write(str - a + "<br/>"); //це теж стрінг, але спершу відбулося віднімання
+document.write(str * "2" + "<br/>"); //аналогічна до попереднього прикладу ситуація
+document.write(str / 2 + "<br/>"); // тут теж
+ */
+
+/*
+//11
+console.log(parseInt("3.14"));   //3
+console.log(parseInt("-7.875")); //-7
+console.log(parseInt("435"));    //435
+console.log(parseInt("Вася"));   //NaN
+ */
+
+/*
+//13
+let odin = +prompt(1);
+let dva = +prompt(2);
+let conc = odin + dva;
+
+alert(`result ${conc}`);
+ */
+
+/*
+//14
+let name1 = prompt('Name?');
+let surname1 = prompt('Surame?');
+let age1 = prompt('Age?');
+
+alert(`Good evening, ${name1} ${surname1},  \ngreetings, you're ${age1} years old`);
+ */
+
+//additional
+
+//1
+// idk how to do this via if...else, so i made it with bubleSort function
+/*
+let numb1 = prompt('1');
+let numb2 = prompt('2');
+let numb3 = prompt('3');
+let arr = [numb1, numb2, numb3];
+
+
+function bubbleSort(arr) {
+    for (var i = 0, endI = arr.length - 1; i < endI; i++) {
+        var wasSwap = false;
+        for (var j = 0, endJ = endI - i; j < endJ; j++) {
+            if (arr[j] > arr[j + 1]) {
+                var swap = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = swap;
+                wasSwap = true;
+            }
+        }
+        if (!wasSwap) break;
+    }
+    return arr;
+}
+bubbleSort(arr);
+
+console.log(arr)
+ */
+
+//2
+
+let color = prompt('Color of lighter?');
+let isRoadClear = confirm('IS ROAD CLEAR?');
+
+ if (color === 'green' || color === 'зелений'){
+     if(isRoadClear) {
+         alert('You can go');
+     } else if(!isRoadClear){
+         alert('wait, while road will be clear')
+     }
+ } else if(color === 'yellow' || color === 'жовтий'){
+     if(isRoadClear) {
+         alert('wait, while road will be clear')
+     } else if(!isRoadClear){
+         alert('wait')
+     }
+ } else if (color === 'red' || color === 'червоний'){
+     if(isRoadClear) {
+         alert('wait still early');
+     } else if(!isRoadClear){
+         alert('stand and wait')
+     }
+ } else{
+     alert('do, what you want');
+ }
