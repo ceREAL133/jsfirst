6b7171838c5e806b41a65d95c22b250cbe50c88c
ceREAL133 <72147047+ceREAL133@users.noreply.github.com>
2021-03-24 13:47:37 +0200

    Update code.js

--- code.js
+++ code.js
@@ -288,11 +288,11 @@ let arr = [numb1, numb2, numb3];
 
 
 function bubbleSort(arr) {
-    for (var i = 0, endI = arr.length - 1; i < endI; i++) {
-        var wasSwap = false;
-        for (var j = 0, endJ = endI - i; j < endJ; j++) {
+    for (let i = 0, endI = arr.length - 1; i < endI; i++) {
+        let wasSwap = false;
+        for (let j = 0, endJ = endI - i; j < endJ; j++) {
             if (arr[j] > arr[j + 1]) {
-                var swap = arr[j];
+                let swap = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = swap;
                 wasSwap = true;
